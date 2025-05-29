dc1c23f7f99f04f062d740561b3358685dc714cc
amoghrgowda <amoghrgowda09@gmail.com>
2025-05-30 00:40:31 +0530

    update 1

--- app/main.py
+++ app/main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return "CI/CD in Python is working!"
 
+@app.route("/abc")
+def alphabets():
+    return "A is for apple, B is for ball"
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
