3673de59a14c8f2da409695c372b745f0252ec0e
amoghrgowda <amoghrgowda09@gmail.com>
2025-05-30 01:17:01 +0530

    added practice tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
 def test_home():
-    from app.main import app
-    client = app.test_client()
+    from app.main import app as app1
+    client = app1.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"CI/CD in Python is working!" in response.data
+    response1 = client.get("/abc")
+    assert response1.status_code == 200
+    assert b"A is for apple, B is for ball, C is for Cat" in response1.data
